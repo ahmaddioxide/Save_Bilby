00004a1c7d4559c8a598d91cc94cd77daab42294
hammadalive7 <200862@students.au.edu.pk>
2023-01-14 01:04:39 +0500

    Splash Screen set with firebase initialization. During Reopening app red screen issue solved.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:save_the_bilby_fund/repository/authentication_repository/authentication_repository.dart';
 // import 'features/authentications/screens/SplashScreen/splash_screen.dart';
+import 'features/authentications/screens/SplashScreen/splash_screen.dart';
 import 'features/authentications/screens/login/login_screen.dart';
 import 'firebase_options.dart';
 import 'package:get/get.dart';
