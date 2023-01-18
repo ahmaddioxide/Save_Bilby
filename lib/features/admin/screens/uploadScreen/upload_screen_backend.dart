1f3fddb8a73a6059e6a0226bb5c0ddb292c20f3e
ahmaddioxide <ahmadmahmood296@gmail.com>
2023-01-18 10:32:05 +0500

    Resolved error of no showing of progress indicator after first upload on upload screen. commented admin side code

--- lib/features/admin/screens/uploadScreen/upload_screen_backend.dart
+++ lib/features/admin/screens/uploadScreen/upload_screen_backend.dart
@@ -7,42 +7,50 @@ import 'package:path/path.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 
+//Controller for upload Screen
 class UploadProgressController extends GetxController {
-  var imagesUploaded = 0.obs;
-  var isUploading = false.obs;
-  void increment() => imagesUploaded++;
-  var isDoneUploading = false.obs;
-  var totalImagesToUpload = 0.obs;
+  var imagesUploaded = 0.obs;//to check how many images have been uploaded
+  var isUploading = false.obs;//to check either you are uploading images or not
+  void increment() => imagesUploaded++;//GetX function to increment imagesUploaded
+  var isDoneUploading = false.obs;//Bool to check if uploading is completed or not
+  var totalImagesToUpload = 0.obs;//count to get how many images is going to upload in a shot
 }
-final uploadProgressController = Get.put(UploadProgressController());
-final ImagePicker imagePicker = ImagePicker();
-List<XFile>? imageFiles = [];
-final imagesDatabaseRef = FirebaseDatabase.instance.ref().child("images");
-final datastoreRef = FirebaseStorage.instance.ref();
+final uploadProgressController = Get.put(UploadProgressController());//crating GetX controller
+final ImagePicker imagePicker = ImagePicker();//image picker instance that pick images from gallery
+List<XFile>? imageFiles = [];//list of files to be uploaded (XFiles)
+final imagesDatabaseRef = FirebaseDatabase.instance.ref().child("images");//images Database reference
+final datastoreRef = FirebaseStorage.instance.ref();//firebase datastore reference\
+
+//function to select images from gallery and upload it to database
 Future SelectAndUploadImages() async {
   final List<XFile>? selectedImages = await imagePicker.pickMultiImage();
-  if (selectedImages!.isNotEmpty) {
+  //if user have selected images
+  if (selectedImages!.isNotEmpty)
+  {
     debugPrint("Number of images selected = ${selectedImages.length}");
     //   int latestImageNumber =await getLastImageNumber();
     uploadProgressController.totalImagesToUpload.value = selectedImages.length;
     for (var image in selectedImages) {
       uploadProgressController.isUploading.value = true;
-      File imageFileToUpload = await File(image.path); // selectedImages as Xfile
+      File imageFileToUpload = await File(image.path); // selectedImages as XFile
       String fileName = await basename(image.path); //getting file name from the path
       fileName = fileName.substring(12, fileName.length - 4); //removing "image_picker" from start and ".jpg" from the end of file name
       fileName=fileName.replaceAll(RegExp('[^A-Za-z0-9]'), '');// removing special characters expect alphabets and numbers
       debugPrint("FILE NAME IS : ${fileName}");
+
       try {
         await datastoreRef
             .child('images/${fileName}')
             .putFile(imageFileToUpload)
             .whenComplete(() async {
-          // imagesUploaded++;
-          uploadProgressController.increment();
+          uploadProgressController.increment();//incrementing when a file is done uploading
           await datastoreRef
+          //uploading images data to firebase realtime database
               .child('images/${fileName}')
               .getDownloadURL()
+          //getting download URL of uploaded image
               .then((imageURL) async {
+                //putting downloadURL to database with name and other attributes
             debugPrint("DOWNLOAD URL: ${imageURL}");
             await imagesDatabaseRef.child("${fileName}").set({
               "imageName": "${fileName}",
@@ -52,10 +60,8 @@ Future SelectAndUploadImages() async {
               "categoryId":"unknown",
             });
           }).whenComplete(() => () {
-            uploadProgressController.imagesUploaded.value=0;
-            uploadProgressController.totalImagesToUpload.value=0;
+//printing image name when done uploading is data to realtime database
                     debugPrint(
-
                         "Image Data saved to Realtime Database: ${fileName}");
                   });
         });
@@ -63,8 +69,11 @@ Future SelectAndUploadImages() async {
         debugPrint("Error in Uploading File $e");
       }
     }
+    //resetting variables
     uploadProgressController.isUploading.value = false;
     uploadProgressController.isDoneUploading.value = true;
+    uploadProgressController.imagesUploaded.value=0;
+    uploadProgressController.totalImagesToUpload.value=0;
   } else {
     debugPrint("No picture Selected");
   }
